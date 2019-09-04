6e31373c92fab86b749f6ec2562599780d894205
Kristaps Vecvagars <vecvagars.kristaps@gmail.com>
2019-09-04 16:59:14 +0300

    added script for mobile footer fix and google font

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- GoogleFont link -->
+        <link href="https://fonts.googleapis.com/css?family=Abel&display=swap" rel="stylesheet">
          <!-- Favicon link -->
         <link rel="shortcut icon" href="#">
          <!-- CSS link -->
--- public/scripts/attributes.js
+++ public/scripts/attributes.js
@@ -2,6 +2,7 @@
 // product types and a switch statement to replace the content of the target <section>
 // based on the selectedIndex of the attribute.
 // The content of the target <section> tag is replaced with a prepared template from templates.js
+hideFooter();
 let attribute_type = document.getElementById('attribute_type');
 let attribute_stats = document.getElementById('attribute_stats');
 
@@ -9,14 +10,36 @@ attribute_type.addEventListener("change", function () {
     switch (true) {
         case attribute_type.selectedIndex == 1:
             attribute_stats.innerHTML = dvd_template;
+            hideFooter();
             break;
 
         case attribute_type.selectedIndex == 2:
             attribute_stats.innerHTML = book_template;
+            hideFooter();
             break;
 
         case attribute_type.selectedIndex == 3:
             attribute_stats.innerHTML = furniture_template;
+            hideFooter();
             break;
     }
-})
+});
+
+function hideFooter() {
+    $(document).ready(function () {
+        var isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+
+        if (isMobile) {
+            let footer = document.querySelector("footer");
+            let inputs = document.querySelectorAll("input");
+            for (let i = 0; i < inputs.length; i++) {
+                inputs[i].addEventListener("blur", function () {
+                    footer.style.opacity = 1;
+                })
+                inputs[i].addEventListener("focus", function () {
+                    footer.style.opacity = 0;
+                })
+            }
+        }
+    });
+};
